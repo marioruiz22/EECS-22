732477ee7db47d93f5c7e15e7dc1e356af2d6c39
marioruiz22 <mariod.jesus22@gmail.com>
2017-07-20 01:23:32 -0700

    Update blackjack.c

--- hw1/blackjack.c
+++ hw1/blackjack.c
@@ -16,44 +16,45 @@ int main(void)
 	card = (rand()%13) + 1;
 	card = (card > 10) ? 10:card;
 
-	if (card ==1) 
-		{
+	if (card == 1) 
+	{
 		printf("Your card is an Ace.\nWould you like its value to be 1 or 11.\n");
 		scanf("%d",&ace);
 		card = (ace == 11) ? 11:card;
-		}
+	}
 	printf("Your first card is:%d \n",card);
 	printf("Do you want another card? \n");
 	printf("Type 1 for Yes, 0 for No: \n");
 	total = total + card;
 	scanf("%d",&choice);
+	
 	while (choice == 1)
 	{
 		card = (rand() %13)+1;
 		card = (card > 10) ? 10:card;
-		if (card ==1)
-			{
-	                printf("Your card is an Ace.\nWould you like its value to be 1 or 11.\n");
-	                scanf("%d",&ace);
-	                card = (ace == 11) ? 11:card;
-                	 }
+		if (card == 1)
+		{
+			printf("Your card is an Ace.\nWould you like its value to be 1 or 11.\n");
+			scanf("%d",&ace);
+			card = (ace == 11) ? 11:card;
+		}
 		printf("Your next card is:%d \n",card);
 		total = total + card;
 		printf("Your combined value is:%d \n",total);
-		if (total >21)
-                {
-                        printf("Sorry. You lose! \n");
-                        x=0;
+		if (total > 21)
+		{
+			printf("Sorry. You lose! \n");
+			x=0;
 			break;
-                }
+		}
 		printf("Do you want another card? \n");
-	        printf("Type 1 for Yes, 0 for No: \n");
+		printf("Type 1 for Yes, 0 for No: \n");
 		scanf("%d",&choice);
 	}
-	while (x ==1)
+	while (x == 1)
 	{
 		card = (rand() %13)+1;
-                card = (card > 10) ? 10:card;
+		card = (card > 10) ? 10:card;
 		dealer_total = dealer_total + card;
 		printf("Dealer draws another card.\n");
 		printf("Dealer's card is:%d\n",card);
@@ -63,7 +64,7 @@ int main(void)
 			printf("Dealer loses. You win! \n");
 			break;
 		}
-		if (dealer_total>=total)
+		if (dealer_total >= total)
 		{
 			printf("Sorry. You lose! \n");
 			break;
